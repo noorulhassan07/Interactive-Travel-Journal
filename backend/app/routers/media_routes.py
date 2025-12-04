64b746b797b6fad78adeb585b1c06d1cbca0e755
Noor ul Hassan <135355225+noorulhassan07@users.noreply.github.com>
2025-12-05 00:36:57 +0500

    Update media_routes.py

--- backend/app/routers/media_routes.py
+++ backend/app/routers/media_routes.py
@@ -1,6 +1,7 @@
+# backend/app/routes/media_router.py
 from fastapi import APIRouter
 
-router = APIRouter()
+router = APIRouter(prefix="/media", tags=["Media"])
 
 @router.get("/")
 async def get_media():
