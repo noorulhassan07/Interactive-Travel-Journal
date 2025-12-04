c609449ad09a20abc427e591cd0e649a23bce763
Noor ul Hassan <135355225+noorulhassan07@users.noreply.github.com>
2025-12-05 00:39:45 +0500

    Update auth_helpers.py

--- backend/app/utils/auth_helpers.py
+++ backend/app/utils/auth_helpers.py
@@ -1,9 +1,9 @@
-# backend/app/utils/auth_helpers.py
 from fastapi import HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from app.utils.security import decode_access_token
 from app.models.user_model import User
 from jose import JWTError
+from beanie import PydanticObjectId
 
 security = HTTPBearer()
 
@@ -16,7 +16,8 @@ async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(s
             raise HTTPException(status_code=401, detail="Invalid authentication credentials")
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid authentication credentials")
-    user = await User.get(user_id)
+
+    user = await User.get(PydanticObjectId(user_id))
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
